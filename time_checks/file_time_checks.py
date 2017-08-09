42eb1a82e6c66698f444e556c6ae3e68f3332d7d
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2017-08-09 15:57:14 +0100

    Add in all single file checks

--- time_checks/file_time_checks.py
+++ time_checks/file_time_checks.py
@@ -11,6 +11,7 @@ import re
 
 from datetime import datetime, timedelta
 from netCDF4 import Dataset, num2date
+from time_checks.test.mock_netcdf import MockNCDataset
 import arrow
 
 from time_checks import time_utils 
@@ -21,14 +22,57 @@ DT_LENGTHS = ['4', '6', '8', '10', '12', '14']
 
 FILE_NAME_PATTERNS_SINGLE = [r'^\d{{{}}}$'.format(dtl) for dtl in DT_LENGTHS]
 FILE_NAME_PATTERNS_DOUBLE = [r'^\d{{{}}}-\d{{{}}}$'.format(dtl, dtl) for dtl in DT_LENGTHS]
+FILE_NAME_REGEXES = [re.compile(pattn) for pattn in (FILE_NAME_PATTERNS_SINGLE
+                                                     + FILE_NAME_PATTERNS_DOUBLE)]
 CMOR_TABLES_FORMAT = {'3hr': 12, '6hrLev': 10, '6hrPLev': 10, 'Amon': 6, 'LImon': 6, 'Lmon': 6, 'OImon': 6,
                       'Omon': 6, 'Oyr': 4, 'aero': 0, 'cf3hr': 12, 'cfDay': 8, 'cfMon': 6, 'cfOff': 0, 'cfSites': 0,
                       'day': 8, 'fx': 0}
-FILE_NAME_REGEXES = [re.compile(pattn) for pattn in (FILE_NAME_PATTERNS_SINGLE
-                                                     + FILE_NAME_PATTERNS_DOUBLE)]
 
 
-def _extract_time_comp(fpath, time_index_in_name=-1, delimiter="_"):
+def _get_nc_attr(var, attr, default=""):
+    if attr in var.ncattrs():
+        return getattr(var, attr)
+    else:
+        return default
+
+
+def _convert_dataset_to_dict(ds):
+
+    """
+    Function that converts from a netCDF4 object to a dictionary in the same form as CEDA-CC.
+    
+    Dictionary is of the form:
+    {"time": {
+        "_type": "float64",
+        "bounds": "time_bnds",
+        "long_name": "time",
+        "standard_name": "time",
+        "units": "days since 1850-01-01",
+        "calendar": "standard",
+        "_data": [52975.5, 53005.0, 53034.5, 53065.0, 53095.5, 53126.0, 53156.5, 53187.5, 53218.0, 53248.5, 53279.0],
+        "axis": "T"},
+        "filename": ["so", "Omon", "MRI-CGCM3", "historical", "r1i1p1", "199501-199912"]
+    """
+
+    # time_var = ds.variables["time"]
+    time_var = time_utils.get_time_variable(ds)
+    d = {
+        "_type": time_var.dtype.name,
+        "bounds": _get_nc_attr(time_var, "bounds"),
+        "long_name": _get_nc_attr(time_var, "long_name"),
+        "standard_name": _get_nc_attr(time_var, "standard_name"),
+        "units": _get_nc_attr(time_var, "units"),
+        "calendar": _get_nc_attr(time_var, "calendar"),
+        "axis": _get_nc_attr(time_var, "axis"),
+        "_data": list(time_var[:])
+    }
+
+    filename_info = os.path.splitext(os.path.basename(ds.filepath()))[0].split("_")
+    return {"time": d, "filename": filename_info}, time_var
+
+
+# THIS FUNCTION IS ONLY REQUIRED IF WORKING DIRECTLY WITH netCDF4 OBJECTS OR MockNCDatasets
+def _extract_filename_component(fpath, index=-1, delimiter="_"):
     """
     Extracts the time component from a file name as a string.
 
@@ -38,26 +82,8 @@ def _extract_time_comp(fpath, time_index_in_name=-1, delimiter="_"):
     :return: time component in file [string]
     """
     fname = os.path.basename(fpath)
-    time_comp = os.path.splitext(fname)[0].split("_")[time_index_in_name]
-    return time_comp
-
-
-def _extract_temporal_frequency(fpath, frequency_index=1, delimiter="_"):
-    """
-    Extracts the temporal frequency from the file name.
-
-    The temporal frequencies are given as the CMIP5 CMOR tables.
-    See https://github.com/PCMDI/cmip5-cmor-tables/tree/master/Tables
-
-    :param fpath: file path [string]
-    :param time_index_in_name: index of time component in the file name [int]
-    :param delimiter: delimiter in file name [string]
-    :return: The time frequency as a string
-    """
-    fname = os.path.basename(fpath)
-    frequency = os.path.splitext(fname)[0].split('_')[frequency_index]
-    return frequency
-
+    fname_comp = os.path.splitext(fname)[0].split(delimiter)[index]
+    return fname_comp
 
 
 def _parse_time(tm):
@@ -118,7 +144,19 @@ def check_file_name_time_format(ds, time_index_in_name=-1):
     :param ds: netCDF4 Dataset object.
     :return: boolean (True for success).
     """
-    time_comp = _extract_time_comp(ds.filepath(), time_index_in_name=time_index_in_name)
+    if isinstance(ds, MockNCDataset):
+        print "MockNCDataset"
+    if isinstance(ds, Dataset):
+        ds, time_var = _convert_dataset_to_dict(ds)
+        # GET REQUIRED INFORMATION FROM DICTIONARY
+        time_comp = ds['filename'][time_index_in_name]
+
+    else: time_comp = _extract_filename_component(ds.filepath(), index=time_index_in_name)
+
+    print time_comp
+
+    # REQUIRED IF WORKING DIRECTLY WITH netCDF4 OBJECTS
+    # time_comp = _extract_filename_component(ds.filepath(), index=time_index_in_name)
 
     for regex in FILE_NAME_REGEXES:
         if regex.match(time_comp):
@@ -139,16 +177,21 @@ def check_file_name_matches_time_var(ds, time_index_in_name=-1, tolerance='days:
     :param tolerance: tolerance of time difference allowed in match [string]
     :return: boolean (True for success).
     """
-    time_comp = _extract_time_comp(ds.filepath(), time_index_in_name=time_index_in_name)
+
+    if isinstance(ds, Dataset): ds, time_var = _convert_dataset_to_dict(ds)
+
+    # REQUIRED IF WORKING DIRECTLY WITH netCDF4 OBJECTS
+    # time_comp = _extract_filename_component(ds.filepath(), index=time_index_in_name)
+    # time_var = time_utils.get_time_variable(ds)
+    # calendar = 'standard'
+    # if 'calendar' in time_var.ncattrs():
+    #     calendar = time_var.calendar
+
+    # GET REQUIRED INFORMATION FROM DICTIONARY
+    time_comp = ds['filename'][time_index_in_name]
+    calendar = ds["time"]["calendar"]
+
     file_times = [_parse_time(comp) for comp in time_comp.split("-")]
-
-    # Get the time variable from the file
-    time_var = time_utils.get_time_variable(ds)
-
-    calendar = 'standard'
-    if 'calendar' in time_var.ncattrs():
-        calendar = time_var.calendar
-
     times = num2date([time_var[0], time_var[-1]], time_var.units, calendar=calendar)
     t_start, t_end = [arrow.get(tm.strftime()) for tm in times]
  
@@ -165,40 +208,37 @@ def check_file_name_matches_time_var(ds, time_index_in_name=-1, tolerance='days:
 def check_time_format_matches_frequency(ds, frequency_index=1, time_index_in_name=-1):
     """
     Checks for consistenty between the time frequency and the format of the time
-    variable_table = "fx": time independent data will not have a temporal element
-    variable_table = "yr": yearly data of the form: yyyy
-    variable_table = "mon": monthly data of the form: yyyyMM
-    variable_table = "monClim": monthly climatology data of the form: yyyyMM
-    variable_table = "day": daily data of the form: yyyyMMdd
-    variable_table = "6hr": 6 hourly data of the form: yyyyMMddhh
-    variable_table = "3hr": 3 hourly data of the form: yyyyMMddhhmm
-    variable_table = "subhr": sub-hourly data of the form: yyyyMMddhhmm
+    variable_table = "fx": time independent data will not have a temporal element NOT IMPLEMENTED YET
+    variable_table = "yr": yearly data of the form: yyyy, test has length 4
+    variable_table = "mon": monthly data of the form: yyyyMM, test has length 6
+    variable_table = "monClim": monthly climatology data of the form: yyyyMM, test has length 6
+    variable_table = "day": daily data of the form: yyyyMMdd, test has length 8
+    variable_table = "6hr": 6 hourly data of the form: yyyyMMddhh, test has length 10
+    variable_table = "3hr": 3 hourly data of the form: yyyyMMddhhmm, test has length 12
+    variable_table = "subhr": sub-hourly data of the form: yyyyMMddhhmm, test has length 12
 
     :param ds:
     :return:
     """
-    
-    time_comp = _extract_time_comp(ds.filepath(), time_index_in_name=time_index_in_name)
-    frequency = _extract_temporal_frequency(ds.filepath(), frequency_index=frequency_index)
-    print time_comp
-    print len(time_comp.split('-')[0])
-    print frequency
+    if isinstance(ds, Dataset):
+        ds = _convert_dataset_to_dict(ds)
+
+        # GET REQUIRED INFORMATION FROM DICTIONARY
+        time_comp = ds['filename'][time_index_in_name]
+        frequency = ds['filename'][frequency_index]
+    else:
+        # REQUIRED IF WORKING WITH netCDF4 objects or MockNCDatasets
+        time_comp = _extract_filename_component(ds.filepath(), index=time_index_in_name)
+        frequency = _extract_filename_component(ds.filepath(), index=frequency_index)
 
     if len(time_comp.split('-')[0]) == CMOR_TABLES_FORMAT[frequency]:
         return True
     
     return False
 
-def check_regular_time_axis_increments(ds):
-    """
-    Checks that the time axis increments are at regular intervals
-    :param ds:
-    :return:
-    """
-    pass
 
 
-def check_valid_temporal_element(element):
+def check_valid_temporal_element(ds, time_index_in_name=-1):
     """
     Checks whether the temporal elements are within the valid ranges:
         years, yyyy: a four digit integer > 0000 (strictly between 1800-2900)
@@ -209,7 +249,70 @@ def check_valid_temporal_element(element):
     :param element:
     :return:
     """
-    pass
+
+    if isinstance(ds, Dataset):
+        ds = _convert_dataset_to_dict(ds)
+
+        # GET REQUIRED INFORMATION FROM DICTIONARY
+        time_comp = ds['filename'][time_index_in_name]
+
+    else:
+        # REQUIRED IF WORKING WITH netCDF4 OBJECTS OR MockNCDatasets
+        time_comp = _extract_filename_component(ds.filepath(), index=time_index_in_name)
+
+    for time_element in time_comp.split('-'):
+        length_time = len(time_element)
+        if length_time >= 4:
+            yyyy = int(time_element[:4])
+            if length_time >= 6:
+                mm = int(time_element[4:6])
+                if length_time >= 8:
+                    dd = int(time_element[6:8])
+                    if length_time >= 10:
+                        hh = int(time_element[8:10])
+                        if length_time >= 12:
+                            mn = int(time_element[10:12])
+
+        if yyyy > 4000 or yyyy < 0000: return False
+
+        if 'mm' in locals():
+            if mm > 12 or mm < 01: return False
+        if 'dd' in locals():
+            if dd > 31 or dd < 01: return False
+            # does not account for calendars where 31 may be invalid for a 360 day calendar
+        if 'hh' in locals():
+            if hh > 23 or hh < 00: return False
+        if 'mn' in locals():
+            if mn > 59 or mn < 00: return False
+
+    return True
+
+def check_regular_time_axis_increments(ds, frequency_index=1):
+    """
+    Checks that the time axis increments are at regular intervals
+    :param ds:
+    :return:
+    """
+
+    if isinstance(ds, Dataset):
+        ds, time_var = _convert_dataset_to_dict(ds)
+
+        calendar = ds["time"]["calendar"]
+        frequency = ds['filename'][frequency_index]
+        times = ds["time"]["_data"]
+
+    delta_t = times[1] -times[0]
+    if frequency == 'mon':
+        # MOnthly frequencies may have irregular intervals test to be implemented
+        pass
+
+        t = 1
+        while t < len(times):
+            t_diff = times[t] - t[t+1]
+            if t_diff != delta_t:
+                return False
+
+    return True
 
 def check_multifile_temporal_continutity(list_of_time_elements):
     """
--- time_checks/test/test_file_time_checks.py
+++ time_checks/test/test_file_time_checks.py
@@ -11,7 +11,6 @@ from time_checks.file_time_checks import *
 from time_checks.test.mock_netcdf import MockNCDataset
 
 
-
 def test_check_file_name_time_format_fail_1():
     eg_names = [
         "mrsos_day_HadGEM2-ES_historical_r1i1p1_19991201-2005.nc",
@@ -42,40 +41,83 @@ def test_check_file_name_matches_time_var_fail_1():
     assert(check_file_name_matches_time_var(ds, time_index_in_name=-1, tolerance='hours:1') == False)
 
 
+def test_check_time_format_matches_frequency_success_1():
+
+    eg_names = ['mrsos_day_HadGEM2-ES_historical_r1i1p1_19991201-20051130.nc',
+                'mrsos_Oyr_HadGEM2-ES_historical_r1i1p1_1999-2005.nc',
+                'mrsos_Amon_HadGEM2-ES_historical_r1i1p1_199912-200511.nc',
+                'mrsos_3hr_HadGEM2-ES_historical_r1i1p1_199912010101-200511300101.nc',
+                'mrsos_6hrLev_HadGEM2-ES_historical_r1i1p1_1999120101-2005113001.nc',
+                ]
+
+    for fname in eg_names:
+        mock_ds = MockNCDataset(fname)
+        assert(check_time_format_matches_frequency(mock_ds, frequency_index=1, time_index_in_name=-1) == True)
+
+
+def test_check_time_format_matches_frequency_fail_1():
+    
+    eg_names = ['mrsos_Oyr_HadGEM2-ES_historical_r1i1p1_19991201-20051130.nc',
+                'mrsos_3hr_HadGEM2-ES_historical_r1i1p1_1999-2005.nc',
+                'mrsos_6hrLev_HadGEM2-ES_historical_r1i1p1_199912-200511.nc',
+                'mrsos_day_HadGEM2-ES_historical_r1i1p1_1999120101-2005113001.nc',
+                'mrsos_Amon_HadGEM2-ES_historical_r1i1p1_199912010101-200511300101.nc',
+                ]
+
+    for fname in eg_names:
+        mock_ds = MockNCDataset(fname)
+        assert(check_time_format_matches_frequency(mock_ds, frequency_index=1, time_index_in_name=-1) == False)
+
+
 def test_check_regular_time_axis_increments_success_1():
-    pass
+    ds = Dataset('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/day/land/day/r1i1p1/latest/mrsos/mrsos_day_HadGEM2-ES_historical_r1i1p1_19991201-20051130.nc')
+    assert(check_regular_time_axis_increments(ds, frequency_index=1) == True)
+
 
 def test_check_regular_time_axis_increments_fail_1():
     pass
 
 
-def test_check_time_format_matches_frequency_success_1():
-    ds = Dataset('/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/historical/day/land/day/r1i1p1/latest/mrsos/mrsos_day_HadGEM2-ES_historical_r1i1p1_19991201-20051130.nc')
-    assert(check_time_format_matches_frequency(ds, frequency_index=1, time_index_in_name=-1) == True)
-
-def test_check_time_format_matches_frequency_fail_1():
-    pass
-
-
 def test_check_valid_temporal_element_success_1():
-    pass
+    eg_names = ['mrsos_day_HadGEM2-ES_historical_r1i1p1_19991201-20051130.nc',
+                'mrsos_Oyr_HadGEM2-ES_historical_r1i1p1_1999-2005.nc',
+                'mrsos_Amon_HadGEM2-ES_historical_r1i1p1_199912-200511.nc',
+                'mrsos_3hr_HadGEM2-ES_historical_r1i1p1_199912010101-200511300101.nc',
+                'mrsos_6hrLev_HadGEM2-ES_historical_r1i1p1_1999120101-2005113001.nc',
+                ]
+
+    for fname in eg_names:
+        mock_ds = MockNCDataset(fname)
+        assert(check_valid_temporal_element(mock_ds, time_index_in_name=-1) == True)
+
 
 
 def test_check_valid_temporal_element_fail_1():
-    pass
+    eg_names = ['mrsos_day_HadGEM2-ES_historical_r1i1p1_19991201-20051132.nc',
+                'mrsos_Oyr_HadGEM2-ES_historical_r1i1p1_1999-4005.nc',
+                'mrsos_Amon_HadGEM2-ES_historical_r1i1p1_199900-200513.nc',
+                'mrsos_3hr_HadGEM2-ES_historical_r1i1p1_199912010100-200511300160.nc',
+                'mrsos_6hrLev_HadGEM2-ES_historical_r1i1p1_1999120100-2005113024.nc',
+                ]
+
+    for fname in eg_names:
+        mock_ds = MockNCDataset(fname)
+        assert(check_valid_temporal_element(mock_ds, time_index_in_name=-1) == False)
 
 
-def test_check_multifile_temporal_continutity_success_1():
-    pass
+# def test_check_multifile_temporal_continutity_success_1():
+#     pass
+#
+#
+# def test_check_multifile_temporal_continutity_fail_1():
+#     pass
+#
+#
+# def test_check_multifile_temporal_completeness_success_1():
+#     pass
+#
+#
+# def test_check_multifile_temporal_completeness_fail_1():
+#     pass
 
 
-def test_check_multifile_temporal_continutity_fail_1():
-    pass
-
-
-def test_check_multifile_temporal_completeness_success_1():
-    pass
-
-
-def test_check_multifile_temporal_completeness_fail_1():
-    pass
