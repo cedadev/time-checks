8f0de72073313a9fa982439eca52c0862f3fdd92
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2017-08-24 15:21:49 +0100

    Update doc strings

--- time_checks/file_time_checks.py
+++ time_checks/file_time_checks.py
@@ -27,9 +27,23 @@ FILE_NAME_REGEXES = [re.compile(pattn) for pattn in (FILE_NAME_PATTERNS_SINGLE
 CMOR_TABLES_FORMAT = {'3hr': 12, '6hrLev': 10, '6hrPLev': 10, 'Amon': 6, 'LImon': 6, 'Lmon': 6, 'OImon': 6,
                       'Omon': 6, 'Oyr': 4, 'aero': 0, 'cf3hr': 12, 'cfDay': 8, 'cfMon': 6, 'cfOff': 0, 'cfSites': 0,
                       'day': 8, 'fx': 0}
-
+IRREGULAR_MONTHLY_CALENDARS = ['gregorian', 'proleptic_gregorian', 'julian', 'noleap', '365_day', 'standard']
+VALID_MONTHLY_TIME_DIFFERENCES = [29.5, 30.5, 31.0]
 
 def _get_nc_attr(var, attr, default=""):
+    """
+    _get_nc_attr
+
+    This function, looks inside a netCDF 4 Dataset object for the attributes of a variable within the object.
+    If the attribute specified for the variable exists then the attribute is returned.
+
+    If the attribute is not associated with the variable then an empty string is returned as default
+
+    :param var: variable name [string]
+    :param attr: attribute name [string]
+    :param default: Default value to be returned [string]
+    :return: variable attribute (default is empty) [string]
+    """
     if attr in var.ncattrs():
         return getattr(var, attr)
     else:
@@ -37,10 +51,11 @@ def _get_nc_attr(var, attr, default=""):
 
 
 def _convert_dataset_to_dict(ds):
-
     """
-    Function that converts from a netCDF4 object to a dictionary in the same form as CEDA-CC.
-    
+        _convert_dataset_to_dict
+
+    This function converts from a netCDF4 object to a dictionary that is in the same form as CEDA-CC.
+
     Dictionary is of the form:
     {"time": {
         "_type": "float64",
@@ -52,6 +67,9 @@ def _convert_dataset_to_dict(ds):
         "_data": [52975.5, 53005.0, 53034.5, 53065.0, 53095.5, 53126.0, 53156.5, 53187.5, 53218.0, 53248.5, 53279.0],
         "axis": "T"},
         "filename": ["so", "Omon", "MRI-CGCM3", "historical", "r1i1p1", "199501-199912"]
+
+    :param ds: a netCDF4 Dataset object [netCDF4 Dataset object]
+    :return: key file metadata [dictionary]
     """
 
     # time_var = ds.variables["time"]
@@ -74,13 +92,16 @@ def _convert_dataset_to_dict(ds):
 # THIS FUNCTION IS ONLY REQUIRED IF WORKING DIRECTLY WITH netCDF4 OBJECTS OR MockNCDatasets
 def _extract_filename_component(fpath, index=-1, delimiter="_"):
     """
-    Extracts the time component from a file name as a string.
+        _extract_filename_component
 
-    :param fpath: file path [string]
-    :param time_index_in_name: index of time component in the file name [int]
-    :param delimiter: delimiter in file name [string]
-    :return: time component in file [string]
+    Extracts a DRS (Data Reference Syntax) component from a file name as a string.
+
+    :param fpath: filepath [string]
+    :param index: index of component in the filename to be retrieved [int]
+    :param delimiter: filename DRS (Data Reference Syntax) delimiter [string]
+    :return: DRS component of filename required [string]
     """
+
     fname = os.path.basename(fpath)
     fname_comp = os.path.splitext(fname)[0].split(delimiter)[index]
     return fname_comp
@@ -88,10 +109,12 @@ def _extract_filename_component(fpath, index=-1, delimiter="_"):
 
 def _parse_time(tm):
     """
-    Parses time component string to date time object.
+        _parse_time
 
-    :param tm: date/time string
-    :return: datetime object
+    Parses time component string to an arrow date time object.
+
+    :param tm: date-time [string]
+    :return: datetime [arrow object]
     """
     # If already formatted then just parse and return
     if "-" in tm:
@@ -100,10 +123,9 @@ def _parse_time(tm):
     # Otherwise, reformat then parse 
     padded_time = tm + "00000101000000"[len(tm):]
     splits = [4, 2, 2, 2, 2, 2]
-    splits.reverse() # so we can pop them off the end
+    splits.reverse()  # so we can pop them off the end
 
     items = []
-
     while padded_time:
         i = splits.pop() 
         item = padded_time[:i]
@@ -116,13 +138,15 @@ def _parse_time(tm):
 
 def _times_match_within_tolerance(t1, t2, tolerance="days:1"):
     """
-    Compares two datetime/arrow objects and returns True if they are 
-    within the time period specified in `tolerance` of each other.
+        _times_match_within_tolerance
+
+    Compares two datetime arrow objects and returns True if they match
+    within the time period specified in `tolerance`.
     
-    :param t1: datetime/arrow object
-    :param t2: datetime/arrow object
+    :param t1: datetime [arrow object]
+    :param t2: datetime [arrow object]
     :param tolerance: tolerance period [string]  
-    :return: boolean [True for success].
+    :return: boolean [True for success]
     """
     unit = tolerance.split(":")[0]
     n = float(tolerance.split(":")[1])
@@ -133,15 +157,46 @@ def _times_match_within_tolerance(t1, t2, tolerance="days:1"):
     if (t1 - delta) < t2 < (t1 + delta):
         return True
 
-    return False 
+    return False
+
+
+def _calculate_detla_time_series(times, valid_dt):
+    """
+       _calculate_detla_time_series
+
+    This function calculates the differences between all the elements of a timeseries and
+    compares the differences with a valid time difference.
+
+    True is returned if all the differences are valid; i.e. equal to the valid time difference argument
+    False is returned if any of the difference fail to match the valid the time difference argument
+
+    :param times: List of times
+    :param valid_dt: Valid time difference, usually scalar, list of valid times supported
+    :return: boolean [True for success]
+    """
+    t = 0
+    while t < len(times) - 1:
+        t_diff = times[t + 1] - times[t]
+        if t_diff not in valid_dt:
+            return False
+        else:
+            return True
 
 
 def check_file_name_time_format(ds, time_index_in_name=-1):
     """
-    Checks that the format of the file name follows matches known
-    regular expressions.
+        check_file_name_time_format
 
-    :param ds: netCDF4 Dataset object.
+    Checks that the format of the file name follows matches known regular expressions of the form YYYY[mm[DD[HH[MM]]]].
+    Where [] indicated the elements are optional and
+    YYYY: is a four digit integer year
+    mm: is a two digit integer month
+    DD: is a two digit integer day
+    HH: is a two digit integer hour
+    MM: is a two digit integer minute
+
+    :param ds: input dataset [netCDF4 Dataset object (also MockNCDataset) or compliant dictionary]
+    :param time_index_in_name: index of time component in the file name [int]
     :return: boolean (True for success).
     """
 
@@ -164,15 +219,17 @@ def check_file_name_time_format(ds, time_index_in_name=-1):
 
 def check_file_name_matches_time_var(ds, time_index_in_name=-1, tolerance='days:1'):
     """
-    Checks that the file name of netCDF4 Dataset `ds` matches the time range
+        check_file_name_matches_time_var
+
+    Checks that the file name of a dataset (file) `ds` matches the time range
     found in the file. The time component in the file name is extracted at
     index `time_index_in_name` when the name is split by "_" and the extension
     removed. The checks are done within the tolerance level specified.
 
-    :param ds: netCDF4 Dataset object.
+    :param ds: input dataset [netCDF4 Dataset object (also MockNCDataset) or compliant dictionary]
     :param time_index_in_name: index of time component in the file name [int]
     :param tolerance: tolerance of time difference allowed in match [string]
-    :return: boolean (True for success).
+    :return: boolean [True for success]
     """
     time_var = time_utils.get_time_variable(ds)
     if type(ds) in settings.supported_datasets: ds = _convert_dataset_to_dict(ds)
@@ -207,19 +264,25 @@ def check_file_name_matches_time_var(ds, time_index_in_name=-1, tolerance='days:
 
 def check_time_format_matches_frequency(ds, frequency_index=1, time_index_in_name=-1):
     """
-    Checks for consistenty between the time frequency and the format of the time
-    variable_table = "fx": time independent data will not have a temporal element NOT IMPLEMENTED YET
-    variable_table = "yr": yearly data of the form: yyyy, test has length 4
-    variable_table = "mon": monthly data of the form: yyyyMM, test has length 6
-    variable_table = "monClim": monthly climatology data of the form: yyyyMM, test has length 6
-    variable_table = "day": daily data of the form: yyyyMMdd, test has length 8
-    variable_table = "6hr": 6 hourly data of the form: yyyyMMddhh, test has length 10
-    variable_table = "3hr": 3 hourly data of the form: yyyyMMddhhmm, test has length 12
-    variable_table = "subhr": sub-hourly data of the form: yyyyMMddhhmm, test has length 12
+        check_time_format_matches_frequency
 
-    :param ds:
-    :return:
+    Checks for consistenty between the time frequency and the format of the time.
+
+    cmor_table = "yr", "Oyr": yearly data of the form: yyyy, test has length 4
+    cmor_table = "Amon", "Lmon", "Omon", "LImon", "OImon", "cfMon": monthly data of the form: yyyyMM, test has length 6
+    cmor_table = "monClim": monthly climatology data of the form: yyyyMM, test has length 6
+    cmor_table = "day", "cfDay": daily data of the form: yyyyMMdd, test has length 8
+    cmor_table = "6hrLev", "6hrPLev": 6 hourly data of the form: yyyyMMddhh, test has length 10
+    cmor_table = "3hr", "": 3 hourly data of the form: yyyyMMddhhmm, test has length 12
+    cmor_table = "subhr": sub-hourly data of the form: yyyyMMddhhmm, test has length 12
+
+
+    NOT IMPLEMENTED YET: 'aero': 0,'cfOff': 0, 'cfSites': 0, 'fx': 0
+
+    :param ds: input dataset [netCDF4 Dataset object (also MockNCDataset) or compliant dictionary]
+    :return: boolean [True for success]
     """
+
     if isinstance(ds, Dataset):
         ds = _convert_dataset_to_dict(ds)
 
@@ -240,14 +303,17 @@ def check_time_format_matches_frequency(ds, frequency_index=1, time_index_in_nam
 
 def check_valid_temporal_element(ds, time_index_in_name=-1):
     """
+        check_valid_temporal_element
     Checks whether the temporal elements are within the valid ranges:
-        years, yyyy: a four digit integer > 0000 (strictly between 1800-2900)
-        months, MM: a two digit integer between 01 and 12
-        days, dd: a two digit integer between 01-31
-        hours, hh: a two digit integer between 00-23
-        minutes, mm: a two digit integer between 00-59
-    :param element:
-    :return:
+    years, yyyy: a four digit integer > 0000 (strictly between 1800-2900)
+    months, MM: a two digit integer between 01 and 12
+    days, dd: a two digit integer between 01-31
+    hours, hh: a two digit integer between 00-23
+    minutes, mm: a two digit integer between 00-59
+
+    :param ds: input dataset [netCDF4 Dataset object (also MockNCDataset) or compliant dictionary]
+    :param time_index_in_name: index of time component in the file name [int]
+    :return: boolean [True for success]
     """
 
     if isinstance(ds, Dataset):
@@ -287,31 +353,23 @@ def check_valid_temporal_element(ds, time_index_in_name=-1):
 
     return True
 
-def _calculate_detla_time_series(times, valid_dt):
-    t = 0
-    while t < len(times)-1:
-        t_diff = times[t+1] - times[t]
-        if t_diff not in valid_dt:
-            return False
-        else:
-            return True
 
 
 def check_regular_time_axis_increments(ds, frequency_index=1):
     """
-    check_regular_time_axis_increments
+       check_regular_time_axis_increments
 
-    This routine checks that the given time axis increments for a given file are regularly spaced throughout.
+    This function checks that the given time axis increments for a given file are regularly spaced throughout.
 
     Since it is common to have the timestamp of monthly data placed at the middle of month,
     monthly CMIP5 maybe irregularly spaced when using any of the following calendars:
         'gregorian', 'proleptic_gregorian', 'julian', 'noleap', '365_day', 'standard',
     For these calendars valid time axis increments are 29.5, 30.5 and 31 days.
 
-    :param ds: netCDF4 Dataset object or compliant dictionary object
+    :param ds: input dataset [netCDF4 Dataset object (also MockNCDataset) or compliant dictionary]
     :param frequency_index: index of the frequency element in the filename
-                            (actually the cmor table, frequency must be implied from this)
-    :return: Boolean True/False
+                            (actually the cmor table, frequency must be implied from this) [int]
+    :return: boolean [True for success]
     """
 
     if isinstance(ds, Dataset):
@@ -323,24 +381,20 @@ def check_regular_time_axis_increments(ds, frequency_index=1):
 
     delta_t = [times[1] -times[0]]
 
-    # For all calendars proleptic_gregorian, noleap, 365_day, standard, gregorian,
-    irregular_mon_cals = ['gregorian', 'proleptic_gregorian', 'julian', 'noleap', '365_day', 'standard']
-    VALID_MONTHLY_TIME_DIFFERENCES = [29.5, 30.5, 31.0]
-
-    if frequency == 'mon' and calendar in irregular_mon_cals:
+    if frequency == 'mon' and calendar in IRREGULAR_MONTHLY_CALENDARS:
         res = _calculate_detla_time_series(times, VALID_MONTHLY_TIME_DIFFERENCES)
 
     else:
         res = _calculate_detla_time_series(times, delta_t)
 
-    return True
+    return res
 
 
 def check_multifile_temporal_continutity(dss, time_index_in_name=-1, frequency_index=1):
     """
-    check_multifile_temporal_continutity:
+       check_multifile_temporal_continutity:
 
-    This routine checks for the temporal continutiy over a given number of datafiles.
+    This function checks for the temporal continutiy over a given number of datafiles.
 
     The test is checks that for each file in a timeseries
     that the start time of given in the filename is one timestep ahead of the end time
@@ -359,7 +413,7 @@ def check_multifile_temporal_continutity(dss, time_index_in_name=-1, frequency_i
     :param frequency_index: index of the frequency element in the filename
                             (actually the cmor table, frequency must be implied from this)
 
-    :return: Boolean True/False
+    :return: boolean [True for success]
     """
 
     # The files must be provided in order at present?
@@ -380,12 +434,8 @@ def check_multifile_temporal_continutity(dss, time_index_in_name=-1, frequency_i
             frequency = _extract_filename_component(ds.filepath(), index=frequency_index)
         file_times.append([_parse_time(comp) for comp in time_comp.split("-")])
 
-  #  import pdb; pdb.set_trace()
-
-
     srt_i = 0; end_i = 1
     ntimes = 0
-    print frequency
     while ntimes < len(file_times) - 1:
         end = file_times[ntimes][end_i]
         start = file_times[ntimes+1][srt_i]
@@ -430,16 +480,3 @@ def check_multifile_temporal_continutity(dss, time_index_in_name=-1, frequency_i
     #
     # return True
 
-
-def check_multifile_temporal_completeness(list_of_time_elements):
-    """
-    Checks the temporal completeness over a list of files.
-    
-    Start to end is continuous
-    For a given experiment is the timeseries complete given the CMIP5 requirements
-
-    :param list_of_time_elements:
-    :return:
-    """
-
-    pass
