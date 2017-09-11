234342372aceb51bfe4553331763dfd38a569185
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2017-09-11 16:11:33 +0100

    Move constants and _<function>s to separate files

--- time_checks/file_time_checks.py
+++ time_checks/file_time_checks.py
@@ -14,22 +14,7 @@ from netCDF4 import Dataset, num2date
 from time_checks.test.mock_netcdf import MockNCDataset
 import arrow
 
-from time_checks import time_utils 
-from time_checks import settings
-
-# File name regular expressions for different time component patterns
-DT_LENGTHS = ['4', '6', '8', '10', '12', '14']
-
-FILE_NAME_PATTERNS_SINGLE = [r'^\d{{{}}}$'.format(dtl) for dtl in DT_LENGTHS]
-FILE_NAME_PATTERNS_DOUBLE = [r'^\d{{{}}}-\d{{{}}}$'.format(dtl, dtl) for dtl in DT_LENGTHS]
-FILE_NAME_REGEXES = [re.compile(pattn) for pattn in (FILE_NAME_PATTERNS_SINGLE
-                                                     + FILE_NAME_PATTERNS_DOUBLE)]
-CMOR_TABLES_FORMAT = {'3hr': 12, '6hrLev': 10, '6hrPLev': 10, 'Amon': 6, 'LImon': 6, 'Lmon': 6, 'OImon': 6,
-                      'Omon': 6, 'Oyr': 4, 'aero': 0, 'cf3hr': 12, 'cfDay': 8, 'cfMon': 6, 'cfOff': 0, 'cfSites': 0,
-                      'day': 8, 'fx': 0}
-IRREGULAR_MONTHLY_CALENDARS = ['gregorian', 'proleptic_gregorian', 'julian', 'noleap', '365_day', 'standard']
-VALID_MONTHLY_TIME_DIFFERENCES = [29.5, 30.5, 31.0]
-
+from time_checks import utils, time_utils, settings
 
 def _resolve_dataset_type(ds):
     """
