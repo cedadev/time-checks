d43c055ee2b66eada30f96136ca80b3f633cf80a
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2017-11-09 08:10:10 +0000

    Update run_file_timechecks

--- time_checks/run_file_timechecks.py
+++ time_checks/run_file_timechecks.py
@@ -29,82 +29,100 @@ from time_checks.file_time_checks import check_valid_temporal_element
 from time_checks.file_time_checks import check_regular_time_axis_increments
 
 
-def test_check_file_name_time_format(w, ds):
+@resolve_dataset_type
+def test_check_file_name_time_format(ds):
 
     if check_file_name_time_format(ds) == False:
-        w.writelines(["T1.001: [check_file_name_time_format]: FAILED:: Format of file name is not recognised", '\n'])
+        return "T1.001: [check_file_name_time_format]: FAILED:: Format of file name is not recognised"
     else:
-        w.writelines(["T1.001: [check_file_name_time_format]: OK", '\n'])
+       return "T1.001: [check_file_name_time_format]: OK"
 
 
-def test_check_valid_temporal_element(w, ds):
+@resolve_dataset_type
+def test_check_valid_temporal_element(ds):
 
     if check_valid_temporal_element(ds, time_index_in_name=-1) == False:
-        w.writelines(["T1.002: [check_valid_temporal_element]: FAILED:: Temporal elements are not valid", '\n'])
+        return "T1.002: [check_valid_temporal_element]: FAILED:: Temporal elements are not valid"
     else:
-        w.writelines(["T1.002: [check_valid_temporal_element]: OK", '\n'])
+       return "T1.002: [check_valid_temporal_element]: OK"
 
 
-def test_check_time_format_matches_frequency(w, ds):
+@resolve_dataset_type
+def test_check_time_format_matches_frequency(ds):
 
     if check_time_format_matches_frequency(ds, frequency_index=1, time_index_in_name=-1) == False:
-        w.writelines(["T1.003: [time_format_matches_frequency]: FAILED:: Frequency element in filename does not match time format in file", '\n'])
+        return "T1.003: [time_format_matches_frequency]: FAILED:: Frequency element in filename does not match time format in file"
     else:
-        w.writelines(["T1.003: [time_format_matches_frequency]: OK", '\n'])
+        return "T1.003: [time_format_matches_frequency]: OK"
 
-def test_check_file_name_matches_time_var(w, ds, frequency):
 
-    table = ds['filename'][1]
+@resolve_dataset_type
+def test_check_file_name_matches_time_var(ds):
+
+
+    table = ds["filename"][1]
     if table in ['Amon', 'Omon', 'Lmon', 'LImon', 'OImon', 'cfMon']:
-        frequency = 'months'
+        frequency = 'days'
         limit = 16
     if table in ['day', 'cfday']:
-        frequency = 'day'
+        frequency = 'days'
         limit = 1
     if table in ['6hrLev', '6hrPlev', '3hr']:
-        frequency = 'hour'
+        frequency = 'hours'
         limit = 1
 
-    tolerance = frequency +':'+ limit
+    tolerance = "{}:{}".format(frequency, limit)
 
-    if check_file_name_matches_time_var(w, ds, time_index_in_name=-1, tolerance=tolerance) == False:
-        w.writelines(["T1.004: [file_name_matches_time_var]: FAILED:: Frequency element in filename does not match time format in file", '\n'])
+    if check_file_name_matches_time_var(ds, time_index_in_name=-1, tolerance=tolerance) == False:
+        return "T1.004: [file_name_matches_time_var]: FAILED:: Frequency element in filename does not match time format in file"
     else:
-        w.writelines(["T1.004: [file_name_matches_time_var]: OK", '\n'])
+        return "T1.004: [file_name_matches_time_var]: OK"
 
-def test_check_regular_time_axis_increments(w, ds):
+
+@resolve_dataset_type
+def test_check_regular_time_axis_increments(ds):
 
     if check_regular_time_axis_increments(ds, frequency_index=1) == False:
-        w.writelines(["T1.005: [regular_time_axis_increments]: FAILED:: Time axis increments are not regular", '\n'])
+        return "T1.005: [regular_time_axis_increments]: FAILED:: Time axis increments are not regular"
     else:
-        w.writelines(["T1.005: [regular_time_axis_increments]: OK", '\n'])
-
+        return "T1.005: [regular_time_axis_increments]: OK"
 
 
 if __name__ == "__main__":
 
+
     #ncfile = "/badc/cmip5/data/cmip5/output1/MOHC/HadGEM2-ES/rcp85/mon/atmos/Amon/r1i1p1/files/tas_20110329/tas_Amon_HadGEM2-ES_rcp85_r1i1p1_200512-203011.nc"
-    files = []
-    for f in argv[1:]:
-        files.append(f)
 
-    for ncfile in files:
+    """
+    Input arguments are
+    1 - file
+    2 - output directory
+    :return:
+    """
 
-        ifile = os.path.basename(ncfile)
-        ofile = ifile.replace('.nc', '__file_timecheck.log')
+    ifile = argv[1]
+    odir = argv[2]
 
-        with open(ofile, 'w') as w:
-            w.writelines(["Time checks of: ", ncfile, "\n"] )
-            if not ifile.endswith('.nc'):
-               w.writelines(["T1.000: [file_extension]: File does not end with '.nc'. \n"])
-               raise Exception("File does not end with '.nc'. Exiting time checks")
-            else:
-                w.writelines(["T1.000: [file_extension]: OK \n"])
+    ncfile = os.path.basename(ifile)
+    ofile = os.path.join(odir, ncfile.replace('.nc', '__file_timecheck.log'))
 
-            ds = Dataset(ncfile)
-            test_check_file_name_time_format(w, ds)
-            test_check_valid_temporal_element(w, ds)
-            test_check_time_format_matches_frequency(w, ds)
-            # test_check_file_name_matches_time_var(w, ds)
-            test_check_regular_time_axis_increments(w, ds)
+    with open(ofile, 'w+') as w:
+        w.writelines(["Time checks of: ", ifile, "\n"] )
+        if not ifile.endswith('.nc'):
+           w.writelines(["T1.000: [file_extension]: File does not end with '.nc'. \n"])
+           raise Exception("File does not end with '.nc'. Exiting time checks")
+        else:
+            w.writelines(["T1.000: [file_extension]: OK \n"])
 
+        ds = Dataset(ifile)
+
+        tests = [test_check_file_name_time_format(ds),
+                 test_check_valid_temporal_element(ds),
+                 test_check_time_format_matches_frequency(ds),
+                 test_check_file_name_matches_time_var(ds),
+                 test_check_regular_time_axis_increments(ds)
+                ]
+
+        for test in tests:
+            res = test
+            w.writelines([res, '\n'])
