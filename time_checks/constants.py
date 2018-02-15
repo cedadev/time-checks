89066221db24c96be2b6ec41797662c3eb1e1901
Ruth Petrie <rpetrie@jasmin-sci1.ceda.ac.uk>
2018-02-15 11:48:57 +0000

    Correct 6hrPLev to 6hrPlev

--- time_checks/constants.py
+++ time_checks/constants.py
@@ -8,7 +8,7 @@ FILE_NAME_PATTERNS_SINGLE = [r'^\d{{{}}}$'.format(dtl) for dtl in DT_LENGTHS]
 FILE_NAME_PATTERNS_DOUBLE = [r'^\d{{{}}}-\d{{{}}}$'.format(dtl, dtl) for dtl in DT_LENGTHS]
 FILE_NAME_REGEXES = [re.compile(pattn) for pattn in (FILE_NAME_PATTERNS_SINGLE
                                                      + FILE_NAME_PATTERNS_DOUBLE)]
-CMOR_TABLES_FORMAT = {'3hr': 12, '6hrLev': 10, '6hrPLev': 10, 'Amon': 6, 'LImon': 6, 'Lmon': 6, 'OImon': 6,
+CMOR_TABLES_FORMAT = {'3hr': 12, '6hrLev': 10, '6hrPlev': 10, 'Amon': 6, 'LImon': 6, 'Lmon': 6, 'OImon': 6,
                       'Omon': 6, 'Oyr': 4, 'aero': 0, 'cf3hr': 12, 'cfDay': 8, 'cfMon': 6, 'cfOff': 0, 'cfSites': 0,
                       'day': 8, 'fx': 0}
 IRREGULAR_MONTHLY_CALENDARS = ['gregorian', 'proleptic_gregorian', 'julian', 'noleap', '365_day', 'standard']
